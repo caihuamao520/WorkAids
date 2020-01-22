4879c4fdf7a5d7d0f500d658ee68aeb8164ac1ae
KxGit <576403245@qq.com>
2020-01-22 16:39:30 +0800

    1、修改了 程序结构 2、添加了其他项目（记事本 音乐）

--- WorkAidsTools/WorkAidsTools/Main.xaml.cs
+++ WorkAidsTools/WorkAidsTools/Main.xaml.cs
@@ -26,10 +26,83 @@ namespace WorkAidsTools
             TimingTaskLis = new Dictionary<string, TimingTaskInfor>();
         }
 
+        private void ShowGridContent(string grindName)
+        {
+            this.grTimingTask.Visibility = Visibility.Collapsed;
+            this.grMusic.Visibility = Visibility.Collapsed;
+            this.grNotepad.Visibility = Visibility.Collapsed;
+
+            switch (grindName)
+            {
+                case "timingTask":
+                    this.grTimingTask.Visibility = Visibility.Visible;
+                    break;
+                case "music":
+                    this.grMusic.Visibility = Visibility.Visible;
+                    break;
+                case "notepad":
+                    this.grNotepad.Visibility = Visibility.Visible;
+                    break;
+            }
+        }
         private void btnTimingTask_Click(object sender, RoutedEventArgs e)
         {
-            TimingTask tt = new TimingTask(TimingTaskLis);
-            tt.ShowDialog();
+            ShowGridContent("timingTask");
         }
+
+        private void btnMusic_Click(object sender, RoutedEventArgs e)
+        {
+            ShowGridContent("music");
+        }
+
+        private void btnNotepad_Click(object sender, RoutedEventArgs e)
+        {
+            ShowGridContent("notepad");
+        }
+
+        #region 定时任务
+        private void btnAdd_Click(object sender, RoutedEventArgs e)
+        {
+            TimingTask_Add ttadd = new TimingTask_Add();
+            if (ttadd.ShowDialog() == true)
+            {
+                string strItenmName = string.Empty;
+                TimingTaskInfor tti = ttadd.TaskInfor;
+                string str = tti.ReminderContent;
+
+                if (string.IsNullOrEmpty(str))
+                {
+                    strItenmName = string.Format("【定时闹钟】时间:{0}", tti.TriggerTime.ToString("yyyy/MM/dd hh:mm:ss"));
+                }
+                else
+                {
+                    strItenmName = string.Format("【备忘提醒 {0}】:{1}", tti.TriggerTime.ToString("yyyy/MM/dd hh:mm:ss"), tti.ReminderContent);
+                }
+                this.ListWorke.Items.Add(strItenmName);
+                TimingTaskLis.Add(strItenmName, tti);
+            }
+        }
+
+        private void btnDelet_Click(object sender, RoutedEventArgs e)
+        {
+            try
+            {
+                object obj = this.ListWorke.SelectedValue;
+
+                if (obj == null) return;
+                string strSelectItemName = obj.ToString();
+
+                TimingTaskLis.Remove(strSelectItemName);
+
+                this.ListWorke.Items.Remove(strSelectItemName);
+            }
+            catch (Exception ee)
+            {
+                MessageBox.Show(ee.Message, "移除异常", MessageBoxButton.OK, MessageBoxImage.Error);
+            }
+        }
+        #endregion
+
+
     }
 }
